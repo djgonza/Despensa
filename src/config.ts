037825af2032718090a765cb1895a9230dec973b
djgonza <davidjimenezgonzalez@MacBook-Pro-de-David.local>
2018-03-20 17:29:20 +0100

    Public

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,5 @@ export default
 	"serverUrl": "https://despensa-rest.herokuapp.com/"
 }
 
-//"https://despensa-rest.herokuapp.com/";
+//"https://despensa-rest.herokuapp.com/"
 //"http://localhost:3000/"
